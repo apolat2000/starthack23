26a4de5004b6695f6bcfa8906ad55664f948bddd
LastLegacy <christopherbastin@live.de>
2023-03-23 18:03:26 +0100

    calculation

---
+++ calculation/stamp_calculation.py
@@ -0,0 +1,10 @@
+"""
+Calculated prize of 11,944444444
+source: https://www.stadt.sg.ch/home/raum-umwelt/abfall-entsorgung/Abfallarten.html
+"""
+
+PRIZE_CONSTANT = 11, 944444444
+
+
+def calculate_prize_of_object(object_volumina):
+    return PRIZE_CONSTANT * object_volumina
